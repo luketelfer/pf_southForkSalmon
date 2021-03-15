7b881f37ee5a3d03ad79c286c2cd11ee68b55523
Luke Telfer <luketelfer@boisestate.edu>
2021-03-15 00:17:11 -0600

    updated bash scripts

--- bash_scripts/ensemble_run/b01_02.sh
+++ bash_scripts/ensemble_run/b01_02.sh
@@ -5,53 +5,29 @@ b="b01"
 run="02"
 
 s="A"
-sbatch -p ${p} -J ${b}-${s}-00 -o ../slurm_out/${b}/${s}/${b}${s}00.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}00
-sbatch -p ${p} -J ${b}-${s}-01 -o ../slurm_out/${b}/${s}/${b}${s}01.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}01
-sbatch -p ${p} -J ${b}-${s}-02 -o ../slurm_out/${b}/${s}/${b}${s}02.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}02
-sbatch -p ${p} -J ${b}-${s}-03 -o ../slurm_out/${b}/${s}/${b}${s}03.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}03
-sbatch -p ${p} -J ${b}-${s}-04 -o ../slurm_out/${b}/${s}/${b}${s}04.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}04
-sbatch -p ${p} -J ${b}-${s}-05 -o ../slurm_out/${b}/${s}/${b}${s}05.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}05
-sbatch -p ${p} -J ${b}-${s}-06 -o ../slurm_out/${b}/${s}/${b}${s}06.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}06
-sbatch -p ${p} -J ${b}-${s}-07 -o ../slurm_out/${b}/${s}/${b}${s}07.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}07
-sbatch -p ${p} -J ${b}-${s}-08 -o ../slurm_out/${b}/${s}/${b}${s}08.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}08
-sbatch -p ${p} -J ${b}-${s}-09 -o ../slurm_out/${b}/${s}/${b}${s}09.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}09
-sbatch -p ${p} -J ${b}-${s}-10 -o ../slurm_out/${b}/${s}/${b}${s}10.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}10
+sbatch -p ${p} -J ${b}-${s}-01 -o ${b}/${s}/${b}${s}01.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}01
+sbatch -p ${p} -J ${b}-${s}-03 -o ${b}/${s}/${b}${s}03.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}03
+sbatch -p ${p} -J ${b}-${s}-05 -o ${b}/${s}/${b}${s}05.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}05
+sbatch -p ${p} -J ${b}-${s}-07 -o ${b}/${s}/${b}${s}07.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}07
+sbatch -p ${p} -J ${b}-${s}-09 -o ${b}/${s}/${b}${s}09.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}09
 
 s="B"
-sbatch -p ${p} -J ${b}-${s}-00 -o ../slurm_out/${b}/${s}/${b}${s}00.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}00
-sbatch -p ${p} -J ${b}-${s}-01 -o ../slurm_out/${b}/${s}/${b}${s}01.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}01
-sbatch -p ${p} -J ${b}-${s}-02 -o ../slurm_out/${b}/${s}/${b}${s}02.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}02
-sbatch -p ${p} -J ${b}-${s}-03 -o ../slurm_out/${b}/${s}/${b}${s}03.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}03
-sbatch -p ${p} -J ${b}-${s}-04 -o ../slurm_out/${b}/${s}/${b}${s}04.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}04
-sbatch -p ${p} -J ${b}-${s}-05 -o ../slurm_out/${b}/${s}/${b}${s}05.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}05
-sbatch -p ${p} -J ${b}-${s}-06 -o ../slurm_out/${b}/${s}/${b}${s}06.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}06
-sbatch -p ${p} -J ${b}-${s}-07 -o ../slurm_out/${b}/${s}/${b}${s}07.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}07
-sbatch -p ${p} -J ${b}-${s}-08 -o ../slurm_out/${b}/${s}/${b}${s}08.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}08
-sbatch -p ${p} -J ${b}-${s}-09 -o ../slurm_out/${b}/${s}/${b}${s}09.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}09
-sbatch -p ${p} -J ${b}-${s}-10 -o ../slurm_out/${b}/${s}/${b}${s}10.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}10
+sbatch -p ${p} -J ${b}-${s}-01 -o ${b}/${s}/${b}${s}01.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}01
+sbatch -p ${p} -J ${b}-${s}-03 -o ${b}/${s}/${b}${s}03.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}03
+sbatch -p ${p} -J ${b}-${s}-05 -o ${b}/${s}/${b}${s}05.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}05
+sbatch -p ${p} -J ${b}-${s}-07 -o ${b}/${s}/${b}${s}07.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}07
+sbatch -p ${p} -J ${b}-${s}-09 -o ${b}/${s}/${b}${s}09.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}09
 
 s="C"
-sbatch -p ${p} -J ${b}-${s}-00 -o ../slurm_out/${b}/${s}/${b}${s}00.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}00
-sbatch -p ${p} -J ${b}-${s}-01 -o ../slurm_out/${b}/${s}/${b}${s}01.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}01
-sbatch -p ${p} -J ${b}-${s}-02 -o ../slurm_out/${b}/${s}/${b}${s}02.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}02
-sbatch -p ${p} -J ${b}-${s}-03 -o ../slurm_out/${b}/${s}/${b}${s}03.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}03
-sbatch -p ${p} -J ${b}-${s}-04 -o ../slurm_out/${b}/${s}/${b}${s}04.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}04
-sbatch -p ${p} -J ${b}-${s}-05 -o ../slurm_out/${b}/${s}/${b}${s}05.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}05
-sbatch -p ${p} -J ${b}-${s}-06 -o ../slurm_out/${b}/${s}/${b}${s}06.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}06
-sbatch -p ${p} -J ${b}-${s}-07 -o ../slurm_out/${b}/${s}/${b}${s}07.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}07
-sbatch -p ${p} -J ${b}-${s}-08 -o ../slurm_out/${b}/${s}/${b}${s}08.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}08
-sbatch -p ${p} -J ${b}-${s}-09 -o ../slurm_out/${b}/${s}/${b}${s}09.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}09
-sbatch -p ${p} -J ${b}-${s}-10 -o ../slurm_out/${b}/${s}/${b}${s}10.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}10
+sbatch -p ${p} -J ${b}-${s}-01 -o ${b}/${s}/${b}${s}01.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}01
+sbatch -p ${p} -J ${b}-${s}-03 -o ${b}/${s}/${b}${s}03.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}03
+sbatch -p ${p} -J ${b}-${s}-05 -o ${b}/${s}/${b}${s}05.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}05
+sbatch -p ${p} -J ${b}-${s}-07 -o ${b}/${s}/${b}${s}07.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}07
+sbatch -p ${p} -J ${b}-${s}-09 -o ${b}/${s}/${b}${s}09.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}09
 
 s="D"
-sbatch -p ${p} -J ${b}-${s}-00 -o ../slurm_out/${b}/${s}/${b}${s}00.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}00
-sbatch -p ${p} -J ${b}-${s}-01 -o ../slurm_out/${b}/${s}/${b}${s}01.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}01
-sbatch -p ${p} -J ${b}-${s}-02 -o ../slurm_out/${b}/${s}/${b}${s}02.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}02
-sbatch -p ${p} -J ${b}-${s}-03 -o ../slurm_out/${b}/${s}/${b}${s}03.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}03
-sbatch -p ${p} -J ${b}-${s}-04 -o ../slurm_out/${b}/${s}/${b}${s}04.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}04
-sbatch -p ${p} -J ${b}-${s}-05 -o ../slurm_out/${b}/${s}/${b}${s}05.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}05
-sbatch -p ${p} -J ${b}-${s}-06 -o ../slurm_out/${b}/${s}/${b}${s}06.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}06
-sbatch -p ${p} -J ${b}-${s}-07 -o ../slurm_out/${b}/${s}/${b}${s}07.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}07
-sbatch -p ${p} -J ${b}-${s}-08 -o ../slurm_out/${b}/${s}/${b}${s}08.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}08
-sbatch -p ${p} -J ${b}-${s}-09 -o ../slurm_out/${b}/${s}/${b}${s}09.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}09
-sbatch -p ${p} -J ${b}-${s}-10 -o ../slurm_out/${b}/${s}/${b}${s}10.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}10
+sbatch -p ${p} -J ${b}-${s}-01 -o ${b}/${s}/${b}${s}01.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}01
+sbatch -p ${p} -J ${b}-${s}-03 -o ${b}/${s}/${b}${s}03.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}03
+sbatch -p ${p} -J ${b}-${s}-05 -o ${b}/${s}/${b}${s}05.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}05
+sbatch -p ${p} -J ${b}-${s}-07 -o ${b}/${s}/${b}${s}07.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}07
+sbatch -p ${p} -J ${b}-${s}-09 -o ${b}/${s}/${b}${s}09.o%j.txt ../ensemble${run}.bash ${b} ${b}${s}09
